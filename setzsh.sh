002f75cd7bfb08fb9763c32e3f13390c7e83939f
ichliebeaya <haruka@kagurazakaharukanoMacBook-Pro.local>
2017-05-10 22:26:31 +0900

    add change permission script to setzsh.sh

--- setzsh.sh
+++ setzsh.sh
@@ -13,3 +13,6 @@ sh ~/dotfiles_macbook/dotfileLinks.sh
 #download dein
 git clone https://github.com/Shougo/dein.vim.git ~/.vim/dein/repos/github.com/Shougo/dein.vim
 
+#change directory permission for mac
+chmod 755 /usr/local/share/zsh/site-functions/
+chmod 755 /usr/local/share/zsh
