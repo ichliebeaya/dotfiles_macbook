310824f3cf695f6c998ac529b4a00659e2f93a01
ichliebeaya <cadmin@cadmin-no-imac.yo>
2017-05-10 15:42:41 +0900

    add setzsh.sh

---
+++ setzsh.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+#change default shell to zsh
+chsh -s /bin/zsh
+
+#install zaw
+cd ~
+git clone git://github.com/zsh-users/zaw.git
+
+#copy .zshrc .zshenv
+sh ~/dotfiles_macbook/dotfileLinks.sh
+
