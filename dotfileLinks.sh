3f483854f830a1fd85d0185dbc90e730a8da56c9
神楽坂はるか <haruka@kagurazakaharukanoMacBook-Pro.local>
2017-06-02 23:05:06 +0900

    add .tmux.conf

--- dotfileLinks.sh
+++ dotfileLinks.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 ln -sf ~/dotfiles_macbook/.vimrc ~/.vimrc
 ln -sf ~/dotfiles_macbook/.vim ~/.vim
-if [ "$SHELL" = "/bin/bash" ];then
-        ln -sf ~/dotfiles_macbook/.bash_profile ~/.bash_profile
-        ln -sf ~/dotfiles_macbook/.bashrc ~/.bashrc
-else if [ "$SHELL" = "/bin/zsh" ];then
-        ln -sf ~/dotfiles_macbook/.zshrc ~/.zshrc
-        ln -sf ~/dotfiles_macbook/.zshenv ~/.zshenv
-     fi
-fi
+ln -sf ~/dotfiles_macbook/.bash_profile ~/.bash_profile
+ln -sf ~/dotfiles_macbook/.bashrc ~/.bashrc
+ln -sf ~/dotfiles_macbook/.zshrc ~/.zshrc
+ln -sf ~/dotfiles_macbook/.zshenv ~/.zshenv
+ln -sf ~/dotfiles_macbook/.tmux.conf ~/.tmux.conf
